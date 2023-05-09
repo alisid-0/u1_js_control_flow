6ffe32b95c93711d16dc97a012d05f770f8e9aa3
Ali Siddiqui <alisiddiquia22@yahoo.com>
2023-05-09 13:13:48 -0500

    created script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+const age = 20
+
+if (age >=18) {
+    //console.log(`you can vote`)
+}
+
+const yourMoney = 200
+const catPrice = 100
+
+if (yourMoney === catPrice){
+    console.log(`You have just enough to buy a cat!`)
+} else if (yourMoney >= catPrice){
+    console.log(`You can buy a cat and will have dollars left over`)
+} else {
+    console.log(`You cannot buy a cat. You need more dollars :(`)
+}
